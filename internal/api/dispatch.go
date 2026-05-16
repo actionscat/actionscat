1f62d84a029cfb0dbb1a2ab36ed068c59500ba64
怪哉GuaiZai <guaizai233@yeah.net>
2026-05-16 22:26:51 +0800

    test: hard-core test action and output logs

--- internal/api/dispatch.go
+++ internal/api/dispatch.go
@@ -3,7 +3,9 @@ package api
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
+	"regexp"
 )
 
 type AdapterRequest struct {
@@ -44,16 +46,33 @@ func DispatchHandler(c *gin.Context) {
 		return
 	}
 
-	//assemble resp data
-	resp := DispatchResponse{
-		OK:     true,
-		Action: "debug_echo",
-		Messages: []ResponseMessage{
-			{
-				Type: "text",
-				Text: fmt.Sprintf("Actioncat core received msg from %s in group %s", req.SenderQQ, req.CurrentGroup),
+	log.Printf(
+		"[adapter] received sender_qq=%s current_group=%s raw_msg=%q",
+		req.SenderQQ,
+		req.CurrentGroup,
+		req.RawMsg,
+	)
+
+	// assemble resp data
+	var resp DispatchResponse
+
+	// 名为 debug_echo 的 action 触发条件为正则匹配字符串“test”
+	matched, _ := regexp.MatchString(`test`, req.RawMsg)
+	if matched {
+		resp = DispatchResponse{
+			OK:     true,
+			Action: "debug_echo",
+			Messages: []ResponseMessage{
+				{
+					Type: "text",
+					Text: fmt.Sprintf("Actioncat core received msg from %s in group %s", req.SenderQQ, req.CurrentGroup),
+				},
 			},
-		},
+		}
+	} else {
+		resp = DispatchResponse{
+			OK: true,
+		}
 	}
 
 	c.JSON(http.StatusOK, resp)
