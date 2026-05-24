8b8726dcc33d42eb35ceaa1b215f24f876c23446
怪哉GuaiZai <guaizai233@yeah.net>
2026-05-24 12:04:10 +0800

    feat(matcher): implement goroutine for Lua execution and time limit

--- internal/matcher/matchlua.go
+++ internal/matcher/matchlua.go
@@ -1,25 +1,45 @@
 package matcher
 
-import lua "github.com/yuin/gopher-lua"
+import (
+	lua "github.com/yuin/gopher-lua"
+	"log"
+	"time"
+)
+
+const LuaExecutionTimeout = 100 * time.Second
 
 func MatchLua(luaPath, msg string) bool {
-	L := lua.NewState()
-	defer L.Close()
+	done := make(chan bool, 1)
+	var result bool
 
-	// set env variable msg
-	L.SetGlobal("msg", lua.LString(msg))
+	go func() {
+		L := lua.NewState()
+		defer L.Close()
 
-	// exec lua file
-	if err := L.DoFile(luaPath); err != nil {
+		// set env variable msg
+		L.SetGlobal("msg", lua.LString(msg))
+
+		// exec lua file
+		if err := L.DoFile(luaPath); err != nil {
+			log.Printf("[matchlua] error executing %s: %v", luaPath, err)
+			done <- false
+			return
+		}
+
+		// get function return value (T/F)
+		if L.GetTop() > 0 {
+			result = L.ToBool(-1)
+			L.Pop(1)
+		}
+		done <- result
+	}()
+
+	// 等待执行完成或超时
+	select {
+	case res := <-done:
+		return res
+	case <-time.After(LuaExecutionTimeout):
+		log.Printf("[matchlua] lua execution timeout for %s (limit: %v)", luaPath, LuaExecutionTimeout)
 		return false
 	}
-
-	// get function return value (T/F)
-	if L.GetTop() > 0 {
-		result := L.ToBool(-1)
-		L.Pop(1)
-		return result
-	}
-
-	return false
 }
